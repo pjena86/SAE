96d9fd3f222e23c890e7ae7094e8c3afc7a1f175
Priya Jena <priyadarsini.jena86@gmail.com>
2021-03-25 20:48:35 -0400

    adding code for runner class

--- src/main/java/com/runner/Runner.java
+++ src/main/java/com/runner/Runner.java
@@ -1,4 +1,12 @@
 package com.runner;
 
-public class Runner {
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features = "src/main/resources/scenarios",
+        glue = "steps")
+
+public class Runner extends AbstractTestNGCucumberTests {
+
+
 }
